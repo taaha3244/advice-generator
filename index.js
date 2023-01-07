b2d6094d06514753d1f1976a4e5f96c3e464846a
taaha3244 <120934618+taaha3244@users.noreply.github.com>
2023-01-07 20:38:07 +0500

    Mobile-version
    Mobile version completed

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 
-const dice=document.querySelector('.dice-container');
-dice.addEventListener('click', async function (e){
+
    
-   
+   const generator=async()=>{
    
         const randomAdvice=document.querySelector('.advice');
     const adviceNumber=document.querySelector('.ad-Num');
         const res=await axios.get('https://api.adviceslip.com/advice')
-        const newAdvice=res.data.slip.advice
+        const newAdvice=`"${res.data.slip.advice}"`
         const newAdviceNumber=res.data.slip.id
         randomAdvice.innerHTML=newAdvice
         adviceNumber.innerHTML=newAdviceNumber
        
-    
-    }
+   }
 
-);
+
+   const dice=document.querySelector('.dice-container');
+dice.addEventListener('click',  function(e){
+    e.preventDefault();
+generator();
+
+
+
+})
